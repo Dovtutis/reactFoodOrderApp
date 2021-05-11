0aef1c5c03a292f66ada3155d5a181abf13a6478
Dovtutis <dovtutis@gmail.com>
2021-05-11 10:44:08 +0300

    checkout form, and firebase db implemented

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -58,6 +58,10 @@ const cartReducer = (state, action) => {
         };
     }
 
+    if (action.type === 'CLEAR') {
+        return defaultCartState;
+    }
+
     return defaultCartState;
 };
 
@@ -72,11 +76,16 @@ const cartReducer = (state, action) => {
             dispatchCartAction({ type: 'REMOVE', id: id });
         };
 
+        const clearCartHandler = () => {
+            dispatchCartAction({type: 'CLEAR'})
+        };
+
         const cartContext = {
             items: cartState.items,
             totalAmount: cartState.totalAmount,
             addItem: addItemToCartHandler,
-            removeItem: removeItemFromCartHandler
+            removeItem: removeItemFromCartHandler,
+            clearCart: clearCartHandler
         };
 
         return (
